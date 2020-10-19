1363e552512215d07e72dc1f0f56546f038f3683
Eduardo Díaz <ediaz@dcc8090.cl>
2020-10-18 22:01:55 -0300

    add difference-of-squares

---
+++ difference-of-squares/src/DifferenceOfSquares.elm
@@ -0,0 +1,18 @@
+module DifferenceOfSquares exposing (difference, squareOfSum, sumOfSquares)
+
+
+squareOfSum : Int -> Int
+squareOfSum n =
+    let
+        sum = (n * (n + 1)) // 2
+    in
+        sum * sum
+
+sumOfSquares : Int -> Int
+sumOfSquares n =
+    (n*(n+1)*(2*n+1)) // 6
+
+
+difference : Int -> Int
+difference n =
+    (squareOfSum n) - (sumOfSquares n)
